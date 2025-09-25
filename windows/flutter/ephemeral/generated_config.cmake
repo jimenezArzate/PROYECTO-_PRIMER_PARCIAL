10e9b53449d66fa1c58a3a6c05a4ae7c80b6c292
jimenezArzate <202222190@tese.edu.mx>
2025-09-25 14:58:23 -0600

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\lupit\\AppData\\Local\\flutter_windows_3.35.4-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\lupit\\OneDrive\\Documentos\\PracLab\\sistema_de_segurida\\flutter_application_Proy\\flutter_application_1\\flutter_application_1" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\lupit\\AppData\\Local\\flutter_windows_3.35.4-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\lupit\\OneDrive\\Documentos\\PracLab\\sistema_de_segurida\\flutter_application_Proy\\flutter_application_1\\flutter_application_1"
+  "FLUTTER_ROOT=C:\\Users\\lupit\\AppData\\Local\\flutter_windows_3.35.4-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\lupit\\OneDrive\\Documentos\\PracLab\\sistema_de_segurida\\flutter_application_Proy\\flutter_application_1\\flutter_application_1\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\lupit\\OneDrive\\Documentos\\PracLab\\sistema_de_segurida\\flutter_application_Proy\\flutter_application_1\\flutter_application_1"
+  "FLUTTER_TARGET=C:\\Users\\lupit\\OneDrive\\Documentos\\PracLab\\sistema_de_segurida\\flutter_application_Proy\\flutter_application_1\\flutter_application_1\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzUuNA==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049ZDY5M2I0YjlkYg==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049YzI5ODA5MTM1MQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My45LjI="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\lupit\\OneDrive\\Documentos\\PracLab\\sistema_de_segurida\\flutter_application_Proy\\flutter_application_1\\flutter_application_1\\.dart_tool\\package_config.json"
+)
